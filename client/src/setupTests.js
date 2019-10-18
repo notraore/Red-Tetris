e17c135afb0f001423e2230e22e05927c8586c2d
Nobila <notraore@student.42.fr>
2019-10-18 18:25:19 +0200

    Test a finir - Nobila

---
+++ client/src/setupTests.js
@@ -0,0 +1,14 @@
+import Adapter from 'enzyme-adapter-react-16'
+import chai from 'chai'
+import { configure as configureEnzyme } from 'enzyme'
+import createChaiEnzyme from 'chai-enzyme'
+import createChaiJestDiff from 'chai-jest-diff'
+import dirtyChai from 'dirty-chai'
+
+
+chai
+  .use(dirtyChai)
+  .use(createChaiJestDiff())
+  .use(createChaiEnzyme())
+
+configureEnzyme({ adapter: new Adapter() })
