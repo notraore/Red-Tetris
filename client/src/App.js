2013db77f7d3b35ecf07cb7f95877a22588960af
Nobila <notraore@student.42.fr>
2019-10-21 23:53:48 +0200

    Test suite

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import { historyPush } from "./history"
 import { gameReducer, initialState } from './reducers/reducer.js'
 import Game from './containers/Game/Game.js'
 
-export const App = props => {
+const App = props => {
 	const [popupInfo, setPopupInfo] = useState(null)
   const [gameState, dispatch] = useReducer(gameReducer, initialState)
 
