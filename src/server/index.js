8ab0375fd1cc49d1e14266234af1d43e592602be
Ektoplasme <jorobin@student.42.fr>
2019-11-26 16:41:55 +0100

    clean before creating testing branch

--- src/server/index.js
+++ src/server/index.js
@@ -18,20 +18,6 @@ app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 })
 
-// fs.readFile("./bible.txt", 'utf8', function(err, data){
-//     if (err){
-//         throw err;
-//     }
-//     var regex = /Rick/gi
-//     var nob = data.replace(regex, 'Summer');
-//     console.log(nob);
-
-//     fs.writeFile("./bible.txt", nob, (err)=> {
-//     	if (err) throw err;
-//     	console.log('File succesfully saved.');
-//     });
-// });
-
 useSockets(io)
 
 server.listen(3000, () => console.log("Port listening on port :3000\n\n█████████████████\n█░░░░░░░░░░░░░░░█\n█░░░░░░░░░░░░░░░█\n█░░████░░░████░░█\n█░░████░░░████░░█\n█░░░░░░███░░░░░░█\n█░░░░███████░░░░█\n█░░░░███████░░░░█\n█░░░░██░░░██░░░░█\n█░░░░░░░░░░░░░░░█\n█████████████████\n───█░░░░░░░░░█───\n───█░░░░░░░░░█───\n───█░░░░░░░░░█───\n───█░░░░░░░░░█───\n───█░░░░░░░░░█───\n───█░░░░░░░░░█───\n───█░░░░░░░░░█───\n───█░░░░░░░░░█───\n───█░░░░░░░░░█───\n───█░░░░░░░░░█───\n───█░░░░░░░░░█───\n█████████████████\n█░░░░░░░█░░░░░░░█\n█░░░░░░░█░░░░░░░█\n█░░░░░░░█░░░░░░░█\n█░░░░░░░█░░░░░░░█\n█████████████████\n█████████████████"))
